aaddb5a0feb0e8815ee9cb94ef8ce7e6b8312702
Ben Briggs <beneb.info@gmail.com>
2015-09-06 16:59:18 +0100

    Bump cssnano to 3.0.

--- tasks/cssnano.js
+++ tasks/cssnano.js
@@ -3,10 +3,15 @@ var cssnano = require('cssnano');
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('cssnano', 'Minify CSS', function () {
+		var cb = this.async();
 		var opts = this.options();
 
-		this.files.forEach(function (el) {
-			grunt.file.write(el.dest, cssnano.process(grunt.file.read(el.src[0]), opts));
-		});
+		Promise.all(this.files.reduce(function (promises, el) {
+			var promise = cssnano.process(grunt.file.read(el.src[0]), opts).then(function (result) {
+				grunt.file.write(el.dest, result.css);
+			});
+			promises.push(promise);
+			return promises;
+		}, [])).then(cb);
 	});
 };
